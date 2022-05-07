20a0405ad9301ab73016498e4e97f2a58c330a4b
Daniel Kloborg <stelfi.gaming@outlook.dk>
2022-05-07 15:42:02 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.Models;
+using System.Linq;
+
+namespace School.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult InsertStudent() {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult InsertStudent(StudentCheck studentCheck) {
+            if (ModelState.IsValid) {
+                Repository.AddResponse(studentCheck);
+                return View("Exit", studentCheck);
+            }
+            else {
+                return View();
+            }
+        }
+        public ViewResult ListStudents() {
+            return View(Repository.Responses.Where(r => r.Semester >= 1 && r.Semester < 6));
+
+        }
+    }
+}
---
+++ Models/StudentCheck.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace School.Models {
+    public class StudentCheck {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify semester")]
+        [Range(1,5)]
+        public int Semester { get; set; }
+    }
+}
